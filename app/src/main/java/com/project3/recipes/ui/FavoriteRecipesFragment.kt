dd810c5709f46059645163fcb1d8dcd3b45de4a3
pehilbert <pehilbert03@gmail.com>
2025-03-25 09:51:56 -0700

    Fragments now actually display

--- app/src/main/java/com/project3/recipes/ui/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/project3/recipes/ui/FavoriteRecipesFragment.kt
@@ -1,7 +1,8 @@
 package com.project3.recipes.ui
 
 import androidx.fragment.app.Fragment
+import com.project3.recipes.R
 
-class FavoriteRecipesFragment : Fragment() {
+class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
 
 }
--- app/src/main/java/com/project3/recipes/ui/RecipeFragment.kt
+++ app/src/main/java/com/project3/recipes/ui/RecipeFragment.kt
@@ -1,7 +1,8 @@
 package com.project3.recipes.ui
 
 import androidx.fragment.app.Fragment
+import com.project3.recipes.R
 
-class RecipeFragment : Fragment() {
+class RecipeFragment : Fragment(R.layout.fragment_recipe) {
 
 }
